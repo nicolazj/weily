4dc471ea7fac7f0cf13eaf9c60597f1d1f7d1848
Nicolas Jiang <nicolazj@gmail.com>
2025-07-05 23:00:54 +1200

    update

--- app/logs.tsx
+++ app/logs.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
-export function getStartOfTodayTimestamp() {
+function getStartOfTodayTimestamp() {
   const today = new Date();
   today.setHours(0, 0, 0, 0); // Set to 00:00:00.000
   return today.getTime(); // Returns milliseconds since epoch
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 import { LogForm } from "./log-form";
 import { Logs } from "./logs";
-function getStartOfTodayTimestamp() {
-  const today = new Date();
-  today.setHours(0, 0, 0, 0); // Set to 00:00:00.000
-  return today.getTime(); // Returns milliseconds since epoch
-}
-
 export default function Home() {
   return (
     <div className="items-center justify-items-center font-[family-name:var(--font-geist-sans)]">
